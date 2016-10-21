bcef598c071a48b0e14e30a5c41761f3cf52c8c1
Andrew Hutchings <andrew@linuxjedi.co.uk>
2016-10-21 07:35:03 +0100

    Fixup for Linux
    * Add a note in the README about libvirt
    * Rip out the libvirt stuff from Vagrantfile
    * Make bootstrap.sh work with the pre-release I provided

--- Vagrantfile
+++ Vagrantfile
@@ -2,9 +2,6 @@ Vagrant.configure("2") do |config|
   config.vm.provider "virtualbox" do |v|
     v.memory = 4096
   end
-  config.vm.provider "libvirt" do |lv|
-    lv.memory = 4096
-  end
   config.vm.hostname = "columnstore.local"
   config.vm.box = "centos/7"
   config.vm.provision :shell, path: "bootstrap.sh"
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ service avahi-daemon start
 yum -y localinstall /vagrant/columnstore/mariadb*.rpm
 
 # run postConfigure with default settings for single server
-/bin/echo -e "1\ncolumnstore-1\n1\n1\n" | /usr/local/mariadb/columnstore/bin/postConfigure
+/bin/echo -e "1\n\n\n\n\n" | /usr/local/mariadb/columnstore/bin/postConfigure
 
 # enable root access in mariadb from host
 /usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'%';FLUSH PRIVILEGES;"
